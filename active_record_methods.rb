2c7e6293b8d87ba260a09ba23c98951a13665696
Douglas Berkley <douglasmberkley@gmail.com>
2023-01-31 18:17:02 +0900

    added last AR methods

--- active_record_methods.rb
+++ active_record_methods.rb
@@ -17,9 +17,10 @@ jules.books
 
 # 6. Create a new book "20000 Leagues under the Seas", it's missing in DB.
 # Store it in a variable: twenty_thousand
-
+twenty_thousand = Book.new(title: '20000 Leagues under the Seas', publishing_year: 1870, category: 'sci-fi')
 
 # 7. Add Jules Verne as this book's author
-
+twenty_thousand.author = jules
 
 # 8. Now save this book in the DB!
+twenty_thousand.save
